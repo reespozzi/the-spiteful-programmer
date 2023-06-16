789db26def22a8e43136a14ead6ebfbc01745460
Rees Pozzi <reespozzi@icloud.com>
2023-06-16 22:36:52 +0100

    Update port and host

--- main.py
+++ main.py
@@ -37,4 +37,7 @@ def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+        # Use the PORT environment variable if available, or fallback to 5000
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
